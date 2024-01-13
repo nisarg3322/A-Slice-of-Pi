99ad8f6382e261f1534eaff56d4547294c4f4ab6
nisarg <patelnisarg.3322@gmail.com>
2024-01-13 01:16:06 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React from 'react';
-import PieComponent from './Components/pieComponent'; // Make sure to use the correct path to your PieChart component
-import Header from './Components/header';
-import BarGraph from './Components/barGraphComponent';
-import MoneyMade from './Components/moneyMadeDisplay';
+import React from "react";
+import PieComponent from "./Components/pieComponent"; // Make sure to use the correct path to your PieChart component
+import Header from "./Components/header";
+import BarGraph from "./Components/barGraphComponent";
+import MoneyMade from "./Components/moneyMadeDisplay";
 
 const App = () => {
   return (
     <>
-    <div style={{ backgroundColor: '#070045', color: 'white', minHeight: '100vh', padding: '20px'}}>
+      <div
+        style={{
+          backgroundColor: "#070045",
+          color: "white",
+          minHeight: "100vh",
+          padding: "20px",
+        }}
+      >
+        <Header />
+        <div
+          style={{
+            display: "flex",
+            gap: "30px",
+            justifyContent: "space-evenly",
+            marginTop: "70px",
+          }}
+        >
+          <PieComponent />
+          <div>
+            <BarGraph />
 
-      <Header />
-      <div style={{display:'flex',gap:'30px',justifyContent:'space-evenly', marginTop:'70px'}}>
-      <PieComponent  />
-      <div>
-      <BarGraph />
-
-      <MoneyMade/>
+            <MoneyMade />
+          </div>
+        </div>
       </div>
-      </div>
-      
-    </div>
-    
     </>
   );
-}
+};
 
 export default App;
